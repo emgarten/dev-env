fb3c3bfc057dd25668a14d17bdc4f602e83487e1
Justin Emgarten <juste@microsoft.com>
2024-06-11 10:34:36 -0700

    Add git cred manager script

---
+++ setup-scripts/windows/git-cred-manager.ps1
@@ -0,0 +1,4 @@
+git config --global credential.azreposCredentialType oauth
+git config --global credential.gitHubAuthModes "oauth,browser"
+git config --global credential.msauthUseBroker true
+git config --global credential.msauthUseDefaultAccount true
