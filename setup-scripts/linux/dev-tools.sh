d8cc178b50ffe01b289e7a0bd82add894c53a127
Justin Emgarten <juste@microsoft.com>
2024-06-24 15:36:44 +0000

    update dev tools script

--- setup-scripts/linux/dev-tools.sh
+++ setup-scripts/linux/dev-tools.sh
@@ -2,5 +2,7 @@
 
 set -xe
 
-sudo apt update
-sudo apt install software-properties-common build-essential cmake git gitk python3 python3-pip python3-venv curl wget unzip gnupg tmux nano apt-transport-https ca-certificates lsb-release -y
+sudo apt update -y
+sudo apt install -y software-properties-common build-essential cmake git gitk \
+    python3 python3-pip python3-venv curl wget unzip gnupg tmux nano \
+    apt-transport-https ca-certificates lsb-release net-tools jq ntpdate
