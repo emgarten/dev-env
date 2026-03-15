f179f8b4eb16cac764c2811b5d9d53710f2eda1e
Justin Emgarten <justin@emgarten.com>
2026-03-14 18:41:58 -0700

    Add fedora scripts for fish

---
+++ setup-scripts/linux/fish-basic-fedora.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -xe
+
+# install fish and required packages
+sudo dnf install fish -y
+
+mkdir -p ~/bin
+mkdir -p ~/.config/fish
+echo "set -g fish_greeting" > ~/.config/fish/config.fish
+echo "set -gx EDITOR nano" >> ~/.config/fish/config.fish
+echo "set -gx VISUAL nano" >> ~/.config/fish/config.fish
+echo "fish_add_path ~/bin" >> ~/.config/fish/config.fish
+echo "fish_add_path ~/.local/bin" >> ~/.config/fish/config.fish
+
+# Set fish as the default shell
+chsh -s /usr/bin/fish
---
+++ setup-scripts/linux/fish-starship-fedora.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -xe
+
+# install fish and required packages
+bash ./fish-basic-fedora.sh
+
+# Install starship
+curl -sS https://starship.rs/install.sh | sh
+
+echo "starship init fish | source" >> ~/.config/fish/config.fish
+
+# for more see https://starship.rs/presets/
+starship preset no-nerd-font -o ~/.config/starship.toml
+
+# If fonts are available on your terminal you can use:
+# starship preset nerd-font-symbols -o ~/.config/starship.toml
+# install the fira code font
+# sudo dnf install unzip -y
+# mkdir -p ~/.locset fish_greetingal/share/fonts
+# pushd ~/.local/share/fonts
+# wget -P ~/.local/share/fonts https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/FiraCode.zip
+# unzip FiraCode.zip
+# rm FiraCode.zip
+# fc-cache -fv
+
+# Plugins
+curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
+omf install bass
+omf install https://github.com/fabioantunes/fish-nvm
